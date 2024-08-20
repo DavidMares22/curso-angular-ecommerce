79bee2e2bdfa5934ed34f0c45ec83fe87354b0cf
David Mares <damr18@gmail.com>
2024-08-20 12:40:13 -0600

    lazy loading

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,18 +1,19 @@
-import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { CounterComponent } from '../../../shared/components/counter/counter.component';
-import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
-import { HighlightDirective } from '../../../shared/directives/highlight.directive';
+import { CommonModule } from '@angular/common';
+
+import { CounterComponent } from '@shared/components/counter/counter.component';
+import { HighlightDirective } from '@shared/directives/highlight.directive';
 import { HeaderComponent } from '@shared/components/header/header.component';
+import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [CommonModule, CounterComponent, WaveAudioComponent, HighlightDirective, HeaderComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
